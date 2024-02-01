bf9a939d4a35e1aa13f400b3e0719d173d8a9d86
Bart Kessels <development@bk-mail.com>
2024-02-01 08:26:51 +0100

    Initialize viewmodel in a correct way

--- app/src/main/kotlin/net/bartkessels/myapplication/MainActivity.kt
+++ app/src/main/kotlin/net/bartkessels/myapplication/MainActivity.kt
@@ -3,12 +3,13 @@ package net.bartkessels.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = MainViewModel()
+        val viewModel: MainViewModel by viewModels()
 
         setContent {
             MaterialTheme {
